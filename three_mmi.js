387c499eed4178cef52c3fecc1c04092e980b5b0
Daniel Blagy <daniel.blagy@gmail.com>
2021-01-19 18:51:55 +0300

    added mouseenter and mouseleave events support

--- three_mmi.js
+++ three_mmi.js
@@ -146,17 +146,32 @@ class MouseMeshInteraction {
 		this.updated = false;
 		this.event = '';
 		
+		// last mesh that the mouse cursor was over
+		this.last_mouseenter_mesh = undefined;
+		
 		this.handlers = new Map();
 		
 		this.handlers.set('click', []);
 		this.handlers.set('dblclick', []);
 		this.handlers.set('contextmenu', []);
 		
+		// not tested, may be broken
+		this.handlers.set('mousedown', []);
+		this.handlers.set('mouseenter', []);
+		this.handlers.set('mouseleave', []);
+		//
+		
 		window.addEventListener('mousemove', this);
 		
 		window.addEventListener('click', this);
 		window.addEventListener('dblclick', this);
 		window.addEventListener('contextmenu', this);
+		
+		// not tested, may be broken
+		window.addEventListener('mousedown', this);
+		//window.addEventListener('mouseenter', this);
+		//window.addEventListener('mouseleave', this);
+		//
 	}
 	
 	handleEvent(e) {
@@ -164,6 +179,8 @@ class MouseMeshInteraction {
 			case "mousemove": {
 				this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
 				this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+				this.updated = true;
+				this.event = 'motion';
 			}
 			break;
 			default: {
@@ -174,9 +191,11 @@ class MouseMeshInteraction {
 	}
 	
 	addHandler(mesh_name, event_type, handler_function) {
+		console.log(mesh_name, '  ', event_type, '  ', handler_function);
 		if (this.handlers.has(event_type)) {
 			this.handlers.get(event_type).push(new MouseMeshInteractionHandler(mesh_name, handler_function));
 		}
+		console.log(this.handlers);
 	}
 	
 	update() {
@@ -188,10 +207,49 @@ class MouseMeshInteraction {
 			const intersects = this.raycaster.intersectObjects(this.scene.children);
 			
 			if (intersects.length > 0) {
-				let handlers_of_event = this.handlers.get(this.event);
-				for (const handler of handlers_of_event) {
-					if (handler.mesh_name === intersects[0].object.name) {
-						handler.handler_function(intersects[0].object);
+				// special test for events: 'mouseenter', 'mouseleave'
+				if (this.event === 'motion') {
+					let mouseenter_handlers = this.handlers.get('mouseenter');
+					let mouseleave_handlers = this.handlers.get('mouseleave');
+					
+					if (mouseenter_handlers.length > 0) {
+						for (const handler of mouseenter_handlers) {
+							if (handler.mesh_name === intersects[0].object.name && intersects[0].object !== this.last_mouseenter_mesh) {
+								this.last_mouseenter_mesh = intersects[0].object;
+								handler.handler_function(intersects[0].object);
+								break;
+							}
+						}
+					}
+					else if (mouseleave_handlers.length > 0) {
+						for (const handler of mouseleave_handlers) {
+							// if mesh was entered by mouse previously, but not anymore, that means it has been mouseleave'd
+							if (handler.mesh_name !== intersects[0].object.name && intersects[0].object === this.last_mouseenter_mesh) {
+								handler.handler_function(intersects[0].object);
+								break;
+							}
+						}
+					}
+				}
+				else {
+					let handlers_of_event = this.handlers.get(this.event);
+					for (const handler of handlers_of_event) {
+						if (handler.mesh_name === intersects[0].object.name) {
+							handler.handler_function(intersects[0].object);
+							break;
+						}
+					}
+				}
+			}
+			// if mouse doesn't intersect any meshes
+			else if (this.event === 'motion') {
+				// special test for 'mouseleave' event
+				// 			(since it may be triggered when cursor doesn't intersect with any meshes)
+				for (const handler of this.handlers.get('mouseleave')) {
+					// if mesh was entered by mouse previously, but not anymore, that means it has been mouseleave'd
+					if (this.last_mouseenter_mesh !== undefined && handler.mesh_name === this.last_mouseenter_mesh.name) {
+						handler.handler_function(this.last_mouseenter_mesh);
+						this.last_mouseenter_mesh = undefined;
 						break;
 					}
 				}
